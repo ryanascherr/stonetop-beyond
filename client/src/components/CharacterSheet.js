2b0503cad0a8d394f982faced8f1f8bfecde77a6
ryanascherr <ryanascherr@gmail.com>
2021-08-16 21:07:13 -0400

    Styling changes to character sheet.

--- client/src/components/CharacterSheet.js
+++ client/src/components/CharacterSheet.js
@@ -20,64 +20,73 @@ const CharacterSheet = () => {
           <div className="three-stats">
                 <div className="character-sheet-stat">
                     <h2>1</h2>
-                    <h3>Armor</h3>
+                    <h3 className="stat-lower">Armor</h3>
                 </div>
                 <div className="character-sheet-stat">
                     <h2>7</h2>
-                    <h3>XP</h3>
+                    <h3 className="stat-lower">XP</h3>
                 </div>
                 <div className="character-sheet-stat">
                     <h2>2</h2>
-                    <h3>Level</h3>
+                    <h3 className="stat-lower">Level</h3>
                 </div>
             </div>
             <div className="two-stats">
                 <div className="character-sheet-stat">
-                    <h2>d6</h2>
-                    <h3>Damage</h3>
+                    <h2 className="stat-number">d6</h2>
+                    <h3 className="stat-lower">Damage</h3>
                 </div>
                 <div className="character-sheet-stat">
                     <h2>18</h2>
-                    <h3>HP (max 18)</h3>
+                    <h3 className="stat-lower">HP (max 18)</h3>
                 </div>
             </div>
             <div className="two-stats">
                 <div className="character-sheet-stat">
-                    <h3>Strength</h3>
-                    <h2>{character.str}</h2>
-                    <h3 className="fixed">(STR)</h3>
+                    <h3 className="stat-higher">Strength</h3>
+                    <h2 className="stat-number">{character.str}</h2>
+                    <h3 className="stat-lower">(STR)</h3>
                 </div>
-                <input type="checkbox" name="weakened" value=""></input>
-                <div className="character-sheet-stat">
-                    <h3>Dexterity</h3>
-                    <h2>{character.dex}</h2>
-                    <h3>(DEX)</h3>
+                <div className="condition-container">
+                    <h3>Weakened</h3>
+                    <input type="checkbox" name="weakened" value=""></input>
                 </div>
-            </div>
-            <div className="two-stats">
                 <div className="character-sheet-stat">
-                    <h3>Intelligence</h3>
-                    <h2>{character.int}</h2>
-                    <h3>(INT)</h3>
-                </div>
-                <input type="checkbox" name="dazed" value=""></input>
-                <div className="character-sheet-stat">
-                    <h3>Wisdom</h3>
-                    <h2>{character.wis}</h2>
-                    <h3>(WIS)</h3>
+                    <h3 className="stat-higher">Dexterity</h3>
+                    <h2 className="stat-number">{character.dex}</h2>
+                    <h3 className="stat-lower">(DEX)</h3>
                 </div>
             </div>
             <div className="two-stats">
                 <div className="character-sheet-stat">
-                    <h3>Constitution</h3>
-                    <h2>{character.con}</h2>
-                    <h3>(CON)</h3>
+                    <h3 className="stat-higher">Intelligence</h3>
+                    <h2 className="stat-number">{character.int}</h2>
+                    <h3 className="stat-lower">(INT)</h3>
                 </div>
-                <input type="checkbox" name="miserable" value=""></input>
+                <div className="condition-container">
+                    <h3>Dazed</h3>
+                    <input type="checkbox" name="weakened" value=""></input>
+                </div>
                 <div className="character-sheet-stat">
-                    <h3>Charisma</h3>
-                    <h2>{character.cha}</h2>
-                    <h3>(CHA)</h3>
+                    <h3 className="stat-higher">Wisdom</h3>
+                    <h2 className="stat-number">{character.wis}</h2>
+                    <h3 className="stat-lower">(WIS)</h3>
+                </div>
+            </div>
+            <div className="two-stats">
+                <div className="character-sheet-stat">
+                    <h3 className="stat-higher">Constitution</h3>
+                    <h2 className="stat-number">{character.con}</h2>
+                    <h3 className="stat-lower">(CON)</h3>
+                </div>
+                <div className="condition-container">
+                    <h3>Miserable</h3>
+                    <input type="checkbox" name="weakened" value=""></input>
+                </div>
+                <div className="character-sheet-stat">
+                    <h3 className="stat-higher">Charisma</h3>
+                    <h2 className="stat-number">{character.cha}</h2>
+                    <h3 className="stat-lower">(CHA)</h3>
                 </div>
             </div>
           </div>
