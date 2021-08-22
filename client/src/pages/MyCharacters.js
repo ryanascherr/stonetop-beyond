6af5925ec03060b1d843a4557557d6a5fec2c029
ryanascherr <ryanascherr@gmail.com>
2021-08-22 18:03:07 -0400

    Removed unneeded comments.

--- client/src/pages/MyCharacters.js
+++ client/src/pages/MyCharacters.js
@@ -8,39 +8,12 @@ import { Link } from 'react-router-dom';
 
 function MyCharacters() {
 
-    // $(document).ready(function () {
-    //     $(".character-btn").click(function () {
-    //       let id = $(this).data("id");
-    //       localStorage.setItem('id', id);
-    //     })
-    //   })
-
-    // let listOfCharacters;
-
-    // let username = localStorage.getItem('username');
-
-    // let { data } = useQuery(QUERY_CHARACTERS, {
-    //     variables: { characterCreator: username }
-    // });
-
-    // const currentCharacter = data?.getCharacters || [];
-
-    // if (currentCharacter.length === 0) {
-    //     listOfCharacters = <h2>You don't have any character yet!</h2>
-    // } else {
-    //     listOfCharacters = currentCharacter.map(character => {
-    //         return <div className="character-card card" key={character._id}>
-    //             <h2>{character.name} {character.playbook}</h2>
-    //             <h2>Level {character.level}</h2>
-    //             <div className="playbook-image-container">
-    //                 <img src={icon} className="playbook-image" alt="Icon"></img>
-    //             </div>
-    //             <Link to={`/sheet`}>
-    //                 <button data-id={character._id} className="character-btn">Select</button>
-    //             </Link>
-    //         </div>
-    //     })
-    // }
+    $(document).ready(function () {
+        $(".character-btn").click(function () {
+          let id = $(this).data("id");
+          localStorage.setItem('id', id);
+        })
+      })
 
     const { username: userParam } = useParams();
 
