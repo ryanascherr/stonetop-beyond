23828a083656e4d88343ca1b96b8695811568dee
ryanascherr <ryanascherr@gmail.com>
2021-09-07 14:00:54 -0400

    Scroll added to character sheet.

--- client/src/pages/CharacterSheet.js
+++ client/src/pages/CharacterSheet.js
@@ -22,32 +22,32 @@ function CharacterSheet({ currentCharacterPage, handleCharacterPageChange })
     localStorage.setItem("playbook", character.playbook);
 
 return (
-    <div className="character-info-tabs">
+    <div className="nav-tabs info-tabs">
         <a
             href="#stats"
             onClick={() => handleCharacterPageChange('Stats')}
-            className={currentCharacterPage === 'Stats' ? 'info-link active' : 'info-link'}
+            className={currentCharacterPage === 'Stats' ? 'nav-link active' : 'nav-link'}
         >
             Stats
         </a>
         <a
             href="#misc"
             onClick={() => handleCharacterPageChange('Misc')}
-            className={currentCharacterPage === 'Misc' ? 'info-link active' : 'info-link'}
+            className={currentCharacterPage === 'Misc' ? 'nav-link active' : 'nav-link'}
         >
             Misc
         </a>
         <a
             href="#moves"
             onClick={() => handleCharacterPageChange('Moves')}
-            className={currentCharacterPage === 'Moves' ? 'info-link active' : 'info-link'}
+            className={currentCharacterPage === 'Moves' ? 'nav-link active' : 'nav-link'}
         >
             Moves
         </a>
         <a
             href="#basic-moves"
             onClick={() => handleCharacterPageChange('BasicMoves')}
-            className={currentCharacterPage === 'BasicMoves' ? 'info-link active' : 'info-link'}
+            className={currentCharacterPage === 'BasicMoves' ? 'nav-link active' : 'nav-link'}
         >
             Basic Moves
         </a>
