33b311bbbcbca0bcf11f89696c4bf4f02f18d44f
ryanascherr <ryanascherr@gmail.com>
2021-09-14 10:15:08 -0400

    Commented out code in connection.js.

--- server/config/connection.js
+++ server/config/connection.js
@@ -9,18 +9,18 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/stonetop', {
 
 module.exports = mongoose.connection;
 
-let sequelize;
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
-}
+// let sequelize;
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// } else {
+//   sequelize = new Sequelize(
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PW,
+//     {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3306,
+//     },
+//   );
+// }
